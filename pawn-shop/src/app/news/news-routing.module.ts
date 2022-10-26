71a322cb6402b849d545f92c1253c29fab6bb101
Trần Hoàng Long <01662121970ax@gmail.com>
2022-10-26 16:20:55 +0700

    final

--- pawn-shop/src/app/news/news-routing.module.ts
+++ pawn-shop/src/app/news/news-routing.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {NewsAddComponent} from "./news-add/news-add.component";
 import {NewsListComponent} from "./news-list/news-list.component";
+import {CheckCanActiveService} from "../service/check-can-active.service";
 
 
 const routes: Routes = [
-  {path: 'news-add', component: NewsAddComponent},
+  {path: 'news-add', component: NewsAddComponent, canActivate: [CheckCanActiveService]},
   {path: 'news-list', component: NewsListComponent}
 ];
 
